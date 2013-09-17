382df9fa5bfda2e2134090cb7673fd6308685a22
Cristian Cavalli <cristiancavalli@cristiancavalli-N82JQ.(none)>
2013-09-17 10:05:20 -0600

    Added functionality for enhanced debug. Uses a combined object of request and respone that is created by an get debuginfo function within the client.

--- lib/DLException.php
+++ lib/DLException.php
@@ -2,16 +2,33 @@
 
 class DLException extends Exception {
 
-    private $url;
+    private $_errorType;
+    private $_request;
+    private $_response;
 
-    public function __construct($statusCode, $response, $url) {
-        $this->code = $statusCode;
-        $this->message = $response;
-        $this->url = $url;
+    public function __construct($debugObject) {
+        $this->_code = $debugObject['response']['http_status'];
+        $this->_errorType = $debugObject['body']['name'];
+        $this->_message = $debugObject['body']['message'];
+        $this->_request = $debugObject['request'];
+        $this->_response = $debugObject['response'];
+        $this->_response['body'] = $debugObject['body'];
     }
 
-    public function getUrl() {
-        return $this->url;
+    public function getErrorMessage() {
+        return $this->_errorType;
+    }
+
+    public function getRequest() {
+        return $this->_request;
+    }
+
+    public function getResponse() {
+        return $this->_response;
+    }
+
+    public function dumpContent() {
+        var_dump($this);
     }
 }
 
