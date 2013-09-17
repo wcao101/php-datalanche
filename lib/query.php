e310d70bf15d7bc66e6569bed7cf4427d2aa731c
Cristian Cavalli <cristiancavalli@cristiancavalli-N82JQ.(none)>
2013-09-17 12:39:36 -0600

    contributes to syntax translation on #2

--- lib/query.php
+++ lib/query.php
@@ -16,40 +16,39 @@ class Query
     public $test_secret;
     */
 
-    public function __construct()
-    {
+    public function __construct() {
+
         $this->_methodType = 'get';
         $this->_baseUrl = '/';
 
         $this->_parameters = array();
 
-        return($this);
+        return $this;
     }
 
-    public function addColumn($object)
-    {
-        if(array_key_exists('add_columns', $this->_parameters) === false)
-        {
-            echo "\nadd was not defined\n";
+    public function addColumn($object) {
+
+        if( array_key_exists('add_columns', $this->_parameters) === false ) {
+
             $this->_parameters['add_columns'] = array();
         }
 
         array_push($this->_parameters['add_columns'], $object);
 
-        return($this);
+        return $this;
     }
 
    
     public function alterColumn($columnName, $object)
     {
-        if(array_key_exists('alter_columns', $this->_parameters) === false)
-        {
+        if( array_key_exists('alter_columns', $this->_parameters) === false ) {
+
             $this->_parameters['alter_column'] = array();
         }
 
         $this->_parameters['alter_columns'][$columnName] = $object;
 
-        return($this);
+        return $this;
     }
 
     public function alterTable($tableName)
@@ -58,14 +57,14 @@ class Query
         $this->_baseUrl = '/alter_table';
         $this->_parameters['table_name'] = $tableName;
 
-        return($this);
+        return $this;
     }
 
     public function columns($objectArray)
     {
         $this->_parameters['columns'] = $objectArray;
 
-        return($this);
+        return $this;
     }
 
     public function createTable($tableName)
@@ -74,7 +73,7 @@ class Query
         $this->_baseUrl = '/create_table';
         $this->_parameters['table_name'] = $tableName;
 
-        return($this);
+        return $this;
     }
 
     public function deleteFrom($tableName)
@@ -83,33 +82,33 @@ class Query
         $this->_baseUrl = '/delete_from';
         $this->_parameters['table_name'] = $tableName;
 
-        return($this);
+        return $this;
     }
 
     public function description($text)
     {
         $this->_parameters['description'] = $text;
 
-        return($this);
+        return $this;
     }
 
     public function distinct($boolean)
     {
         $this->_parameters['distinct'] = $boolean;
 
-        return($this);
+        return $this;
     }
 
     public function dropColumn($columnName)
     {
-        if(array_key_exists('drop_columns', $this->_parameters) === false)
-        {
+        if( array_key_exists('drop_columns', $this->_parameters) === false ) {
+
             $this->_parameters['drop_columns'] = array();
         }
 
         array_push($this->_parameters['drop_columns'], $columnName);
 
-        return($this);
+        return $this;
     }
 
     public function dropTable($tableName)
@@ -118,14 +117,14 @@ class Query
         $this->_baseUrl = '/drop_table';
         $this->_parameters['table_name'] = $tableName;
 
-        return($this);
+        return $this;
     }
 
     public function from($table)
     {
         $this->_parameters['from'] = $table;
 
-        return($this);
+        return $this;
     }
 
     public function getTableInfo($tableName)
@@ -134,7 +133,7 @@ class Query
         $this->_baseUrl = '/get_table_info';
         $this->_parameters['table_name'] = $tableName;
 
-        return($this);
+        return $this;
     }
 
     public function getTableList()
@@ -142,13 +141,13 @@ class Query
         $this->_methodType = 'get';
         $this->_baseUrl = '/get_table_list';
 
-        return($this);
+        return $this;
     }
     public function groupBy($columns)
     {
         $this->_parameters['group_by'] = $columns;
 
-        return($this);
+        return $this;
     }
 
     public function insertInto($tableName)
@@ -157,49 +156,49 @@ class Query
         $this->_baseUrl = '/insert_into';
         $this->_parameters['table_name'] = $tableName;
 
-        return($this);
+        return $this;
     }
 
     public function isPrivate($boolean)
     {
         $this->_parameters['is_private'] = $boolean;
 
-        return($this);
+        return $this;
     }
 
     public function license($object)
     {
         $this->_parameters['license'] = $object;
 
-        return($this);
+        return $this;
     }
 
     public function limit($integer)
     {
         $this->_parameters['limit'] = $integer;
 
-        return($this);
+        return $this;
     }
 
     public function offset($integer)
     {
         $this->_parameters['offset'] = $integer;
 
-        return($this);
+        return $this;
     }
 
     public function orderBy($objectArray)
     {
         $this->_parameters['order_by'] = $objectArray;
 
-        return($this);
+        return $this;
     }
 
     public function rename($tableName)
     {
         $this->_parameters['rename'] = $tableName;
 
-        return($this);
+        return $this;
     }
 
     public function select($columns)
@@ -208,28 +207,28 @@ class Query
         $this->_baseUrl = '/select_from';
         $this->_parameters['select'] = $columns;
 
-        return($this);
+        return $this;
     }
 
     public function set($map)
     {
         $this->_parameters['set'] = $map;
 
-        return($this);
+        return $this;
     }
 
     public function sources($objectArray)
     {
         $this->_parameters['sources'] = $objectArray;
 
-        return($this);
+        return $this;
     }
 
     public function total($boolean)
     {
         $this->_parameters['total'] = $boolean;
 
-        return($this);
+        return $this;
     }
 
     public function update($tableName)
@@ -238,14 +237,14 @@ class Query
         $this->_baseUrl = '/update';
         $this->_parameters['table_name'] = $tableName;
 
-        return($this);
+        return $this;
     }
 
     public function values($rows)
     {
         $this->_parameters['values'] = $rows;
 
-        return($this);
+        return $this;
     }
 
     public function where($filter)
@@ -257,22 +256,22 @@ class Query
             $this->_parameters['where'] = $filter;        
         }
 
-        return($this);
+        return $this;
     }
 
     public function getMethodType()
     {
-        return($this->_methodType);
+        return $this->_methodType;
     }
 
     public function getBaseUrl()
     {
-        return($this->_baseUrl);
+        return $this->_baseUrl;
     }
 
     public function getParameters()
     {
-        return($this->_parameters);
+        return $this->_parameters;
     }
     public function setMethod($method)
     {
