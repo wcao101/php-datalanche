76668fe7a591be2a50346bc04e45c04c0bf351ba
Cristian Cavalli <cristiancavalli@cristiancavalli-N82JQ.(none)>
2013-09-18 16:34:17 -0600

    Further addresses #16, begining to implement zend style documentation

--- lib/DLClient.php
+++ lib/DLClient.php
@@ -16,12 +16,34 @@ include 'DLExpression.php';
 include 'DLException.php';
 
 
-/*
-\
-*Class DLClient contains the overwhelming amount of functionality
-*regarding connection creation, control and closure.
-/
+/**
+* \\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\
+* Class DLClient contains the overwhelming amount of functionality
+* regarding connection creation, control and closure.
+* DLC client does post and get parsing for provided input.
+* Post bodies are formatted using json_encode functionality while
+* get parameters are serialzed into a url string using basic
+* associative encoding procedure. Upon instantiation of a 
+* new DLClient automatic execution procedure (__construct) 
+* expects several set-up inputs for succesful client configuration. 
+* Specifically: your api sercret, api key, desired host, 
+* port, and whether or not you would like to strictly use secure (https)
+* connections for interacting with the specified host. 
+* Disabling '$_verifySsl' (that is setting it to false or null)
+* is ideal for development procedures or self-signed certs.
+*
+* @category     raw PHP/Datalanche
+* @package      datalanche
+* @subpackage   client
+* @copyright    Copyright (c) 2013 Datalanche (https://www.datalanche.com/)
+* @license      https://www.datalanche.com/terms
+* @version      Release: @package_version@
+* @link         https://github.com/datalanche/php-datalanche.git
+* @since        @release_version@
+* @deprecated   still in use
+* //////////////////////////////////////////////////
 */
+
 class DLClient 
 {
     private $_parameters;
@@ -32,92 +54,139 @@ class DLClient
     private $_connection;
     private $_getUrl;
     private $_host;
-    private $_curlStatusArray;
 
-
+    /**
+    * Inital execution construct for client class.
+    * Configures and parses basic varaible submission
+    * for the set-up of client <-> database interaction.
+    *
+    * @access public
+    * @param string $secret
+    * @param string $key
+    * @param string $host
+    * @param string|int $port
+    * @param bool|string $ssl
+    * @return mixed/object return $this or this object upon completion
+    */
     public function __construct ($secret, $key, $host, $port, $ssl)
     {
-        //assignments
-        $url = 'https://';
-        $this->_curlStatusArray = array();
+        /*BASIC ASSIGNMENTS*/
+        $url = 'https://';  //Preface the url string with the intended protocol.
 
+        /* BEGIN BASIC LOGIC TO DETERMINE ARGUMEENTS*/
+
+        //Testing for whether a custom host was provided
         if(($host === null)
             || ($host === '')
         ) {
-
-            $this->_host = 'api.datalanche.com';
-
+            //If there isn't a custom host, revert to datalanche
+            $this->_host = 'api.datalanche.com'; 
         } else {
-
-            $this->_host = $host;
-
+            //Otherwise we are using a custom host.
+            $this->_host = $host; 
         }
 
-        $url = $url.$host;
+        $url = $url.$host; //Now we'll take the url string and append the host to the end of it.
 
+        //Testing to see whether the use has provided a custom port
         if (($port === null)
             || ($port === '')
         ) {
-
-            $this->_port = null;
-
+            //If there is no provided port, default to settings used by Datalanche
+            $this->_port = null; 
         } else {
-
-            $this->_port = $port;
+            //Otherwise get the port and make the needed modification to the url string
+            $this->_port = $port; 
             $url = $url.':'.$port;
-
         }
 
+        //Now we have the full base url, so we can go ahead and assign it to the object.
         $this->_url = $url;
 
+        //Checking to make sure to verify ssl certs.
         if (($ssl === false)
             || ($ssl === 0)
             || ($ssl === 'false')
         ) {
-
-            $this->verify_ssl = $ssl;
-
+            $this->_verifySsl = false; //Verify ssl is off, unverfied certification possible.
         } elseif(($ssl === null)
             || ($ssl === true) 
             || ($ssl === 1) 
             || ($ssl === 'true')
         ) {
-
+            /* If verify ssl is null (default value if undeclared) then we default
+            to using secure and verified communications; we do the same if explicitly
+            qualified as true as well. */
             $this->_verifySsl = true;
         }
 
+        /*Now testing for provided api key & secret.
+        If these value are null then they are left unset (null).*/
         if($secret != null) {
-
             $this->_secret = $secret; 
-
         }
         if ($key != null) {
-
             $this->_key = $key;
-
         }
 
+        /*
+        *RETURN THE OBJECT AFTER EXECUTION
+        */
         return $this;
     }
 
+    /**
+    * httpAssocEncopde is built as a stripped down alternative to the built
+    * in functionality of http_build_query which does not set null entries
+    * during url encoding, and therefore is not compliant with datalanche
+    * testing/operating SOP. The function expects a one dimensional (depth
+    * of one) array of keyed values. They key is then encoded as the variable
+    * name and the accompaying array value slot is set as the value of the (new)
+    * get param (encoded simply as a string for url use). This function is
+    * unoptimized and not tested for signifigant load.
+    *
+    * @access private
+    * @param array $array a single dimension (depth of one) array which contains a list of keyed values
+    * @return string $requestOption a string of compiled get arguments
+    */
     private function httpAssocEncode($array)
     {
-        $requestOption = '';
-        $end = end($array);
+        $requestOption = ''; //this is the intended holder of the final string, set it to nothing first.
+        $end = end($array); //get the end of the array
 
-        foreach($array as $key => $entry) {
+        /*
+        * I disclaim the following functionality as a temporary, hacky way 
+        * to finsih the intended task, simple and stripped down as it is.
+        */
 
+        foreach($array as $key => $entry) { 
+            /*
+            * Iterate through the entries and check to see if they are null.
+            * The following check and assignment is the only reason this code
+            * exists as http_build_query regards keyed values in an array set
+            * to null as non-existent, therefore not allowing unset variables.
+            */
+
+            /*
+            * The following check & assigment is redundant. PHP, though, might be better off 
+            * setting $entry = '' Running perscribed tests with either value does not 
+            * produce diffrences in results or execution since either values looks the
+            * same on the get param line e.g. localhost:4001?var_one=&var_two=value
+            * where var_one is the entry that was set to null when passed to client.
+            */
             if($entry === null) {
-
-                $entry = '';
+                $entry = null;
             }
             
+            /*
+            * Check to see if we are at the end of the array
+            * if we are then we won't append the '&' because
+            * we are at the end of the params list.
+            */
             if($entry == $end) {
-
                 $requestOption = (string) $requestOption.urlencode($key).'='.urlencode($entry);
 
             } else {
-
                 $requestOption = (string) $requestOption.urlencode($key).'='.urlencode($entry).'&';
             }
 
@@ -131,15 +200,33 @@ class DLClient
         return $requestOption;
     }
 
+    /**
+    * curlCreator is a functionalization of basic curl
+    * params which are consistent across request types -
+    * (get/post/[get]delete). From the programs last major version iteration
+    * curl construction templates have been changed to include header request
+    * and response info, necessitating further functionality to parse how
+    * curl decides to store and read this information.
+    *
+    * @access private
+    * @return mixed/resource $curlHandle this is the current curl handle, used to formulate and execute requests
+    * @uses curl_init() a curl resource to being a curl connection
+    * @uses curl_setopt_array() a curl resource which allows the setting of parameters through an array
+    */
+
     private function curlCreator()
     {
+        /*
+        * The options array 
+        * WARNING: changing a value in this array will change all related values across request types
+        */
         $options = array (
             CURLOPT_HEADER => true,
             CURLOPT_ENCODING => 'gzip',
             CURLOPT_HTTPAUTH => CURLAUTH_BASIC,
             CURLOPT_SSLVERSION => 3,
-            CURLOPT_SSL_VERIFYPEER => false,
-            CURLOPT_SSL_VERIFYHOST => false,
+            CURLOPT_SSL_VERIFYPEER => $this->_verifySsl,
+            CURLOPT_SSL_VERIFYHOST => $this->_verifySsl,
             CURLOPT_FORBID_REUSE => false,
             CURLOPT_USERAGENT => 'Datalanche PHP Client',
             CURLOPT_VERBOSE => false,
@@ -148,17 +235,37 @@ class DLClient
             CURLINFO_HEADER_OUT => true
             );
 
-        $curlHandle = curl_init();
+        /**
+        * @var resource/curl contains the curl handle
+        */
+        $curlHandle = curl_init(); //We are initalizing the curl connection here
+        
+        //Give it the values we just laid out
         curl_setopt_array($curlHandle, $options);
 
         return $curlHandle;
     }
 
+    /**
+    * resultsMediator was built off the need to functionalize the parsing methods
+    * required to synthesize the headers and response body from the semi unhelpful datatype
+    * that the curl libraries return by default (a giant string).
+    * resultsMediator explodes the results and requests into keyed arrays contained by
+    * a singular array which the user can then refrence to get request/response headers,
+    * general info, and response body content. This function also contains the json error
+    * check which is used to throw specific errors at the json_decode level.
+    *
+    * @access private
+    * @param string $serverResponseString the results of $this->handleResults(curl_exec($curlHandle)) [the response of the server to the request]
+    * @param array $curlInfoArray the results of $this->handleResults(curl_get_info($curlHandle)) POST execution of the handle
+    * @uses json_decode() the only place in client where this function is called and checked for errors
+    * @uses json_last_error() part of a switch which throws specific json errors
+    * @return array $statusArray is an array of arrays contating info about the curl response & request
+    */
+
     private function resultsMediator($serverResponseString, $curlInfoArray)
     {
-        //var_dump($serverResponseString);
-        //var_dump($curlInfoArray);
-        //exit();
+        // Unecessary but helpful to demonstrate the basic structure of the return object
         $statusArray = array();
         $statusArray['request'] = array();
         $statusArray['response'] = array();
@@ -166,13 +273,25 @@ class DLClient
         $statusArray['response']['header'] = array();
         $statusArray['response']['body'] = array();
 
+        //Get the header minus the possible body
         $responseHeader = substr($serverResponseString, 0, $curlInfoArray['header_size']);
+        //whatever is left over is the response body
         $responseBody = substr($serverResponseString, $curlInfoArray['header_size']);
+        //Explode the header across the newlines, which is the way each header value slot is seperated
         $responseHeader = explode("\n", $responseHeader);
+        //Header status will always be the 0th entry
         $statusArray['response']['header']['status'] = $responseHeader[0];
 
         array_shift($responseHeader);
 
+        /*
+        * Loop over the rest of the header and explode along the colon.
+        * Take the content before the colon and assign it as a new array key
+        * while taking the content after the colon and assigning it as that key's
+        * value.
+        *
+        * This radically simplifies access to any given part of response or request.
+        */
         foreach($responseHeader as $value)
         {
             $middle = explode(":", $value);
@@ -192,8 +311,16 @@ class DLClient
             }
         }
 
+        /*
+        * Decode the response body and recursivley set any objects to assosiative arrays.
+        */ 
         $responseBody = json_decode($responseBody, true);
 
+        /*
+        * Check for errors related to json decoding,
+        * this helps to isolate specific errors with
+        * json responses from the server.
+        */
         switch( json_last_error() ) {
             case JSON_ERROR_DEPTH:
                 throw new Exception('JSON error: Maximum stack depth has been exceeded');
@@ -212,7 +339,7 @@ class DLClient
                 break;
 
             case JSON_ERROR_UTF8:
-                throw new Exception('JSON error: Malformed UTF_* charachters, possibly incorrectly encoded');
+                throw new Exception('JSON error: Malformed UTF-8 charachters, possibly incorrectly encoded');
                 break;
             case JSON_ERROR_NONE:
             default:
@@ -221,6 +348,12 @@ class DLClient
 
         $statusArray['response']['body'] = $responseBody;
         $statusArray['response']['header']['http_code'] = $curlInfoArray['http_code'];
+
+        /*
+        * We move to do the same process again but this time on the request headers
+        * we won't have to parse for a json body this time either, so the 
+        * process is a bit shorter.
+        */
 
         $requestHeader = explode("\n", $curlInfoArray['request_header']);
         $statusArray['request']['header']['operation'] = explode(" ", $requestHeader[0]);
@@ -241,10 +374,22 @@ class DLClient
                 $statusArray['request']['header'][trim($middle[0])] = trim($middle[1]);
             }
         }
+
+        /* 
+        * Appending the entierty of the curl_get_info
+        * in its untouched, unprocessed version since some
+        * of the info in there might be helpful to debugging
+        * and development
+        */ 
+
         $statusArray['curl_info_array'] = $curlInfoArray;
 
         return $statusArray;
     }
+
+    /**
+    * BOOKMARK
+    */
 
     private function getBody($query)
     {
