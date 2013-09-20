9cf304f282b5f82f26019d6886c641f129df8af2
Cristian Cavalli <cristiancavalli@cristiancavalli-N82JQ.(none)>
2013-09-20 12:53:57 -0600

    inital add

---
+++ examples/create-table.php
@@ -0,0 +1,60 @@
+<?php
+include '../lib/client.php';
+
+class CreateTableExample
+{
+    public function __construct($secret, $key, $host, $port, $ssl)
+    {
+        $result = null;
+        $client = new Client($secret, $key, $host, $port, $ssl);
+        $query = new Query();
+        $query->createTable('my_table');
+        $query->description('my_table description text');
+        $query->isPrivate(true);
+        $query->license(
+                array(
+                    'name' => 'license_name',
+                    'url' => 'http://license.com',
+                    'description' => 'license description text'
+                    ));
+        $query->sources(
+                array(
+                    array(
+                        'name' => 'source1',
+                        'url' => 'http://source1.com',
+                        'description' => 'source1 description text'
+                        ),
+                    array(
+                        'name' => 'source2',
+                        'url' => 'http://source2.com',
+                        'description' => 'source2 description text'
+                        )
+                    ));
+        $query->columns(
+                array(
+                    array(
+                        'name' => 'col1',
+                        'data_type' => 'uuid',
+                        'description' => 'col1 description text'
+                        ),
+                    array(
+                        'name' => 'col2',
+                        'data_type' => 'timestamp',
+                        'description' => 'col2 description text'
+                        ),
+                    array(
+                        'name' => 'col3',
+                        'data_type' => 'string',
+                        'description' => 'col3 description text'
+                        )
+                    ));
+            $results = $client->query($query);
+            echo "\n----\n";
+            var_dump($results);
+            echo "\n----\n";
+    }
+}
+
+$createTableExample = new CreateTableExample('VCBA1hLyS2mYdrL6kO/iKQ==','7zNN1Pl9SQ6lNZwYe9mtQw==', 'localhost', 4001, false);
+
+?>
