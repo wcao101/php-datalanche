c5b3d0a3834d5cce00d1773beac08b55c374f09b
Cristian Cavalli <cristiancavalli@cristiancavalli-N82JQ.(none)>
2013-09-20 13:54:37 -0600

    updated to dl prefix, passes

--- examples/update.php
+++ examples/update.php
@@ -1,17 +1,17 @@
 <?php
 
-include "../lib/client.php";
+include "../lib/DLClient.php";
 
 class UpdateExample
 {
     public function __construct($secret, $key, $host, $port, $ssl)
     {
         $results = null;
-        $client = new Client($secret, $key, $host, $port, $ssl);
+        $client = new DLClient($secret, $key, $host, $port, $ssl);
         $expression = new DLExpression();
         $expression->column('col3')->equals('hello');
 
-        $query = new Query();
+        $query = new DLQuery();
         $query->update('my_table');
         $query->set(array('col3' => 'hello world'));
         $query->where($expression);
