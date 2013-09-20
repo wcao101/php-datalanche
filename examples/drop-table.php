d6854c4e9b14ffb36e75aed87973a964d5acd1ea
Cristian Cavalli <cristiancavalli@cristiancavalli-N82JQ.(none)>
2013-09-20 15:30:02 -0600

    removed internal execution, passes

--- examples/drop-table.php
+++ examples/drop-table.php
@@ -1,23 +1,31 @@
 <?php
 
-include "../lib/DLClient.php";
 
 class DropTableExample
 {
-    public function __construct($secret, $key, $host, $port, $ssl)
+    public function __construct($secret, $key, $host, $port, $ssl, $tableName)
     {
         $results = null;
         $client = new DLClient($secret, $key, $host, $port, $ssl);
         $query = new DLQuery();
-        $query->dropTable('my_table');
+        $query->dropTable($tableName);
 
         $results = $client->query($query);
-        echo "\n----\n";
-        var_dump($results);
-        echo "\n----\n";
+        echo "----\n";
+        echo "drop-table:\n";
+            echo "code: ".$results['response']['headers']['http_code']."\n";
+            if($results['response']['headers']['http_code'] === 200)
+            {
+                echo "!! PASS !!\n";
+                echo "----\n";
+                return true;
+            } else {
+                echo "!! FAIL !!\n";
+                echo "----\n";
+                return false;
+            }
     }
 }
 
-$dropTableExample = new DropTableExample('VCBA1hLyS2mYdrL6kO/iKQ==','7zNN1Pl9SQ6lNZwYe9mtQw==', 'localhost', 4001, false);
 
 ?>
