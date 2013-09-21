46b29f6f68aef104f2f6ff4cc36bd00216af08ca
Cristian Cavalli <cristiancavalli@cristiancavalli-N82JQ.(none)>
2013-09-20 18:05:16 -0600

    fixes #39, fixes #38, fixes #37

--- examples/get-table-info.php
+++ examples/get-table-info.php
@@ -1,9 +1,8 @@
 <?php
 
+require_once(dirname(__FILE__) . '/../Datalanche.php');
 
-class GetTableInfoExample
-{
-    public function __construct($secret, $key, $host, $port, $ssl)
+    function DLGetTableInfo($secret, $key, $host, $port, $ssl)
     {
         $results = null;
         $client = new DLClient($secret, $key, $host, $port, $ssl);
@@ -11,23 +10,13 @@ class GetTableInfoExample
         $query->getTableInfo('my_table');
 
 
-        $results = $client->query($query);
-
-        echo "----\n";
-        echo "get-table-info:\n";
-            echo "code: ".$results['response']['headers']['http_code']."\n";
-            if($results['response']['headers']['http_code'] === 200)
-            {
-                echo "!! PASS !!\n";
-                echo "----\n";
-                return true;
-            } else {
-                echo "!! FAIL !!\n";
-                echo "----\n";
-                return false;
-            }
+        try
+        {
+            $results = $client->query($query);
+        } catch (Exception $e) {
+            echo $e."\n";
+        }
     }
-}
 
 
 ?>
