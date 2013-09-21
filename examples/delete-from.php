34fe569648a760036ba1a9468e672836938253c7
Cristian Cavalli <cristiancavalli@cristiancavalli-N82JQ.(none)>
2013-09-20 18:04:14 -0600

    fixes #39, fixes #38, fixes #37

--- examples/delete-from.php
+++ examples/delete-from.php
@@ -1,9 +1,8 @@
 <?php
 
+require_once(dirname(__FILE__) . '/../Datalanche.php');
 
-class DeleteFromExample
-{
-    public function __construct($secret, $key, $host, $port, $ssl)
+    function DLDeleteFrom($secret, $key, $host, $port, $ssl)
     {
         $results = null;
         $client = new DLClient($secret, $key, $host, $port, $ssl);
@@ -13,23 +12,13 @@ class DeleteFromExample
         $query->deleteFrom('my_table');
         $query->where($columnComparison);
 
-        $results = $client->query($query);
-
-        echo "----\n";
-        echo "delete-from-table\n";
-            echo "code: ".$results['response']['headers']['http_code']."\n";
-            if($results['response']['headers']['http_code'] === 200)
-            {
-                echo "!! PASS !!\n";
-                echo "----\n";
-                return true;
-            } else {
-                echo "!! FAIL !!\n";
-                echo "----\n";
-                return false;
-            }
+        try
+        {
+            $results = $client->query($query);
+        } catch (Exception $e) {
+            echo $e."\n";
+        }
     }
-}
 
 
 ?>
