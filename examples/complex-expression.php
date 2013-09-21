6f36208da616de8e2f81146553a3874e55ed5824
Cristian Cavalli <cristiancavalli@cristiancavalli-N82JQ.(none)>
2013-09-20 18:15:30 -0600

    removed class stuff, reworked

--- examples/complex-expression.php
+++ examples/complex-expression.php
@@ -1,37 +1,39 @@
 <?php
 
-include "../lib/client.php";
+require_once(dirname(__FILE__) . '/../Datalanche.php');
 
-class ComplexExpressionExample
-{
-    public function __construct($secret, $key, $host, $port, $ssl)
+    function DLComplexExpression($secret, $key, $host, $port, $ssl)
     {
         $results = null;
-        $client = new Client($secret, $key, $host, $port, $ssl);
+        $client = new DLClient($secret, $key, $host, $port, $ssl);
         $expression = new DLExpression();
         $expression->boolAnd(
             array(
-               $this->returnNewExpression()->boolOr(
+               returnNewExpression()->boolOr(
                     array(
-                        $this->returnNewExpression()->column('col3')->equals('hello'),
-                        $this->returnNewExpression()->column('col3')->equals('world')
+                        returnNewExpression()->column('col3')->equals('hello'),
+                        returnNewExpression()->column('col3')->equals('world')
                         )),
-                $this->returnNewExpression()->column('col1')->equals('0f21b968-cd28-4d8b-9ea6-33dbcd517ec5')
+                returnNewExpression()->column('col1')->equals('0f21b968-cd28-4d8b-9ea6-33dbcd517ec5')
             ));
-        $query = new Query();
+        $query = new DLQuery();
         $query->select('*')->from('my_table')->where($expression);
         $results = $client->query($query);
 
-        echo "\n----\n";
-        var_dump($results);
-        echo "\n----\n";
+         try
+        {
+            $results = $client->query($query);
+        } catch (Exception $e) {
+            echo $e."\n";
+        }
+
     }
 
-    private function returnNewExpression()
+    function returnNewExpression()
     {
         return new DLExpression();
     }
-}
 
-$complexExpressionExample = new ComplexExpressionExample('VCBA1hLyS2mYdrL6kO/iKQ==','7zNN1Pl9SQ6lNZwYe9mtQw==', 'localhost', 4001, false);
+DLComplexExpression('VCBA1hLyS2mYdrL6kO/iKQ==','7zNN1Pl9SQ6lNZwYe9mtQw==', 'localhost', 4001, false);
+
 ?>
