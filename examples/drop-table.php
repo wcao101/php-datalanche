777a9996df0e85e78824ec6fabb766f88610e835
Cristian Cavalli <cristiancavalli@cristiancavalli-N82JQ.(none)>
2013-09-20 18:04:39 -0600

    fixes #39, fixes #38, fixes #37

--- examples/drop-table.php
+++ examples/drop-table.php
@@ -1,31 +1,21 @@
 <?php
 
+require_once(dirname(__FILE__) . '/../Datalanche.php');
 
-class DropTableExample
-{
-    public function __construct($secret, $key, $host, $port, $ssl, $tableName)
+    function DLDropTable($secret, $key, $host, $port, $ssl, $tableName)
     {
         $results = null;
         $client = new DLClient($secret, $key, $host, $port, $ssl);
         $query = new DLQuery();
         $query->dropTable($tableName);
 
-        $results = $client->query($query);
-        echo "----\n";
-        echo "drop-table:\n";
-            echo "code: ".$results['response']['headers']['http_code']."\n";
-            if($results['response']['headers']['http_code'] === 200)
-            {
-                echo "!! PASS !!\n";
-                echo "----\n";
-                return true;
-            } else {
-                echo "!! FAIL !!\n";
-                echo "----\n";
-                return false;
-            }
+        try
+        {
+            $results = $client->query($query);
+        } catch (Exception $e) {
+            echo $e."\n";
+        }
     }
-}
 
 
 ?>
