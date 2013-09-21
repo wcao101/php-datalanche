269d743ae00b6211cacca9d18840dd152d73c4e5
Cristian Cavalli <cristiancavalli@cristiancavalli-N82JQ.(none)>
2013-09-20 18:06:13 -0600

    fixes #39, fixes #38, fixes #37

--- examples/update.php
+++ examples/update.php
@@ -1,9 +1,9 @@
 <?php
 
+require_once(dirname(__FILE__) . '/../Datalanche.php');
 
-class UpdateExample
-{
-    public function __construct($secret, $key, $host, $port, $ssl)
+
+    function DLUpdate($secret, $key, $host, $port, $ssl)
     {
         $results = null;
         $client = new DLClient($secret, $key, $host, $port, $ssl);
@@ -14,24 +14,13 @@ class UpdateExample
         $query->update('my_table');
         $query->set(array('col3' => 'hello world'));
         $query->where($expression);
-
-        $results = $client->query($query);
-
-        echo "----\n";
-        echo "update-table:\n";
-            echo "code: ".$results['response']['headers']['http_code']."\n";
-            if($results['response']['headers']['http_code'] === 200)
-            {
-                echo "!! PASS !!\n";
-                echo "----\n";
-                return true;
-            } else {
-                echo "!! FAIL !!\n";
-                echo "----\n";
-                return false;
-            }
+        try
+        {
+            $results = $client->query($query);
+        } catch (Exception $e) {
+            echo $e."\n";
+        }
     }
-}
 
 
 ?>
