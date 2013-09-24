cb78ac378f1f0b0a779705b5173f14c76675f395
Cristian Cavalli <cristiancavalli@cristiancavalli-N82JQ.(none)>
2013-09-24 12:57:10 -0600

    #42 reviewed and corrected formatting, documentation added, exception handling expanded

--- examples/complex-expression.php
+++ examples/complex-expression.php
@@ -2,43 +2,42 @@
 
 require_once(dirname(__FILE__) . '/../Datalanche.php');
 
-$apiKey = 'your_api_key';
-$apiSecret = 'your_api_secret';
-
-
+/**
+* @return object DLExpression Cleanly refrence a new instance of DLExpression
+*/
 function returnNewExpression()
 {
     return new DLExpression();
 }
 
+try {
+    /**
+    * @uses DLClient add your API key and API secret
+    */
+    $apiKey = 'your_api_key';
+    $apiSecret = 'your_api_secret';
+    $client = new DLClient($apiSecret, $apiKey);
 
-$results = null;
-$client = new DLClient($apiSecret, $apiKey);
-$expression = new DLExpression();
-$expression->boolAnd(
-array(
-    returnNewExpression()->boolOr(
-        array(
-                returnNewExpression()->column('col3')->equals('hello'),
-                returnNewExpression()->column('col3')->equals('world')
-            )),
-            returnNewExpression()->column('col1')->equals('0f21b968-cd28-4d8b-9ea6-33dbcd517ec5')
-    ));
+    $expression = new DLExpression();
+    $expression->boolAnd(
+    array(
+        returnNewExpression()->boolOr(
+            array(
+                    returnNewExpression()->column('col3')->equals('hello'),
+                    returnNewExpression()->column('col3')->equals('world')
+                )),
+                returnNewExpression()->column('col1')->
+                    equals('0f21b968-cd28-4d8b-9ea6-33dbcd517ec5')
+        ));
 
-$query = new DLQuery();
-$query->select('*')->from('my_table')->where($expression);
-$results = $client->query($query);
+    $query = new DLQuery();
+    $query->select('*')->from('my_table')->where($expression);
 
-try
-{
     $results = $client->query($query);
-} catch (Exception $e) {
+
+} catch (DLException $e) {
+    echo $e."\n";
+} catch (Exception $ex) {
     echo $e."\n";
 }
-
-if($results['response']['headers']['http_code'] === 200)
-{
-    echo "SUCCESS!!\n";
-}
-
 ?>
