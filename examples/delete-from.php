256901a35bcb27de06e5d2a4190f2fd50d9fb63f
Cristian Cavalli <cristiancavalli@cristiancavalli-N82JQ.(none)>
2013-09-24 13:27:06 -0600

    #42 reviewed and corrected formatting, documentation added, exception handling expanded

--- examples/delete-from.php
+++ examples/delete-from.php
@@ -1,28 +1,34 @@
 <?php
 
 require_once(dirname(__FILE__) . '/../Datalanche.php');
-$apiKey = 'your_api_key';
-$apiSecret = 'your_api_secret';
 
+try {
+    /**
+    * @uses DLClient add your API secret and API key
+    */
+    $apiKey = 'your_api_key';
+    $apiSecret = 'your_api_secret';
+    $client = new DLClient($apiSecret, $apiKey);
 
-$results = null;
-$client = new DLClient($apiSecret, $apiKey);
-$columnComparison = new DLExpression();
-$columnComparison->column('col3')->equals('hello');
-$query = new DLQuery();
-$query->deleteFrom('my_table');
-$query->where($columnComparison);
+    $columnComparison = new DLExpression();
+    $columnComparison->column('col3')->equals('hello');
 
-try
-{
+    /**
+    * @uses DLQuery::deleteFrom() Only $query->deleteFrom() is required
+    *
+    * $query->where() is optional however, all rows will be deleted from
+    * the specified table if the where clause is missing.
+    */
+    $query = new DLQuery();
+    $query->deleteFrom('my_table');
+    $query->where($columnComparison);
+
     $results = $client->query($query);
-} catch (Exception $e) {
-    echo $e."\n";
-}
 
-if($results['response']['headers']['http_code'] === 200)
-{
-    echo "SUCCESS!!\n";
+} catch (DLException $e) {
+    echo $e."\n";
+} catch (Exception $ex) {
+    echo $ex."\n";
 }
 
 
