82da74dab678cc559372ae3cc23671743ad6d216
Cristian Cavalli <cristiancavalli@cristiancavalli-N82JQ.(none)>
2013-09-24 13:39:48 -0600

    #42 reviewed and corrected formatting, documentation added, exception handling expanded

--- examples/get-table-list.php
+++ examples/get-table-list.php
@@ -1,27 +1,23 @@
 <?php
 
 require_once(dirname(__FILE__) . '/../Datalanche.php');
-$apiKey = 'your_api_key';
-$apiSecret = 'your_api_secret';
 
+try{
+    /**
+    * @uses DLClient Add your API secret and API key
+    */
+    $apiKey = 'your_api_key';
+    $apiSecret = 'your_api_secret';
+    $client = new DLClient($apiSecret, $apiKey);
 
+    $query = new DLQuery();
+    $query->getTableList();
 
-$results = null;
-$client = new DLClient($apiSecret, $apiKey);
-$query = new DLQuery();
-$query->getTableList();
-
-
-try
-{
     $results = $client->query($query);
-} catch (Exception $e) {
+} catch (DLException $e) {
     echo $e."\n";
-}
-
-if($results['response']['headers']['http_code'] === 200)
-{
-    echo "SUCCESS!!\n";
+} catch (Exception $ex) {
+    echo $ex."\n";
 }
 
 ?>
